fd461f41842d78d20271bee1d68b84a3a73c0e04
SmolChungus <mpamuller@msn.com>
2021-10-25 19:45:31 +0200

    yasreil

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -16,8 +16,6 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (32.0f));
-    g.setColour (juce::Colours::white);
     g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 }
 
